4cba7170da425097841e47d33a069a748f88f420
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-09-08 22:17:07 -0600

    Update the Home page for user flow and calendar page in Upcoming Event in user flow

--- app/memberFlow/page.js
+++ app/memberFlow/page.js
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { useUserContext } from "../context/userContext";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import MessagePage from "../components/MessagePage";
 
 function PageContent() {
 
@@ -18,13 +19,29 @@ function PageContent() {
     }
     console.log("User: ", user);
   }, []);
+
+  const MOCK_MESSAGES = [
+    {
+        id: 1,
+        name: "John Doe",
+        message: "Sure! You can see my available time on the booking management",
+        date: "Jun 15, 2025",
+    },
+    // dummy data → total 50 messages
+    ...Array.from({ length: 49 }, (_, i) => ({
+        id: i + 2,
+        name: `User Dummy ${i + 1}`,
+        message: "Yes, you are right about the job application, i will have a …",
+        date: "Jun 15, 2025",
+    })),
+    ];
   
   return (
     <>
       <Navbar />
       {/* Page content */}
       <main className="bg-gray-100 min-h-screen">
-        <div className="container mx-auto px-4 py-8">
+        {/* <div className="container mx-auto px-4 py-8">
           <h1 className="text-2xl font-bold text-center mb-6 text-orange-500">
             Welcome to Tech Connect Alberta
           </h1>
@@ -37,7 +54,12 @@ function PageContent() {
           >
             Go to Testing Page
           </Link>
+        </div> */}
+
+        <div className="p-10">
+          <MessagePage messageList={MOCK_MESSAGES} />
         </div>
+        
       </main>
     </>
   );
