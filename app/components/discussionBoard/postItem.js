7911d13212a094168d307f504f008fd8adffb2bf
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-10-14 22:30:54 -0600

    Update the UI design for discussion board

--- app/components/discussionBoard/postItem.js
+++ app/components/discussionBoard/postItem.js
@@ -1,15 +1,16 @@
 import React from "react";
 
-export default function PostItem({ author, title, onClick }) {
+export default function PostItem({ author, title, onClick, disabled=false }) {
   return (
-    <div 
-      className="border-b border-gray-200 pb-4 cursor-pointer hover:bg-gray-50 p-2 rounded-md" 
+    <button 
+      className="w-full border-b border-gray-200 pb-4 cursor-pointer hover:bg-gray-50 p-2 rounded-md disabled:bg-[#E2B596]" 
       onClick={onClick}
+      disabled={disabled}
     >
-      <div className="flex justify-between items-center">
-        <p className="font-semibold text-black">{author}</p>
+      <div className="flex justify-start items-start">
+        <p className="text-left font-semibold text-black">{author}</p>
       </div>
-      <p className="text-gray-700 text-sm mt-1">{title}</p>
-    </div>
+      <p className="text-left text-gray-700 text-sm mt-1">{title}</p>
+    </button>
   );
 }
