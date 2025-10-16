3cf2404cae01cd2882d28fcade1561804c3d9a0e
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-10-15 23:53:42 -0600

    Update client management UI in advisor dashboard

--- app/advisorDashboard/clientRow.js
+++ app/advisorDashboard/clientRow.js
@@ -4,65 +4,68 @@ import { useEffect, useState } from "react";
 
 
 export default function ClientRow({
-    sessionID,
-    name,
-    message,
-    subtitle,
+    client,
     onMessage,
 }) {
 
-    const [subtitleStyle, setSubtitleStyle] = useState("");
-    const [selectedValue, setSelectedValue] = useState(subtitle || "active");
+    const [statusStyle, setStatusStyle] = useState("");
+    const [selectedStatus, setSelectedStatus] = useState(client.status || "active");
     const [expanded, setExpanded] = useState(false);
 
 
     useEffect(() => {
-        if(!selectedValue) return;
+        if(!selectedStatus) return;
 
-        if(selectedValue === 'active') {
-            setSubtitleStyle("px-2 py-1 rounded bg-blue-100 text-blue-700");
-        } else if(selectedValue === 'closed') {
-            setSubtitleStyle("px-2 py-1 rounded bg-red-100 text-red-700");
-        } else if(selectedValue === 'pending') {
-            setSubtitleStyle("px-2 py-1 rounded bg-yellow-100 text-yellow-700");
+        if(selectedStatus === 'active') {
+            setStatusStyle("px-2 py-1 rounded bg-blue-100 text-blue-700");
+        } else if(selectedStatus === 'closed') {
+            setStatusStyle("px-2 py-1 rounded bg-red-100 text-red-700");
+        } else if(selectedStatus === 'pending') {
+            setStatusStyle("px-2 py-1 rounded bg-yellow-100 text-yellow-700");
         }
-    }, [selectedValue]);
+    }, [selectedStatus]);
 
     useEffect(() => {
-        if(!subtitle) return;
+        if(!selectedStatus) return;
 
-        if(subtitle === 'active') {
-            setSubtitleStyle("px-2 py-1 rounded bg-blue-100 text-blue-700");
+        if(selectedStatus === 'active') {
+            setStatusStyle("rounded bg-blue-100 text-blue-700");
         }
 
-        if(subtitle === 'closed') {
-            setSubtitleStyle("px-2 py-1 rounded bg-red-100 text-red-700");
+        if(selectedStatus === 'closed') {
+            setStatusStyle("rounded bg-red-100 text-red-700");
         }
 
-        if(subtitle === 'pending') {
-            setSubtitleStyle("px-2 py-1 rounded bg-yellow-100 text-yellow-700");
+        if(selectedStatus === 'pending') {
+            setStatusStyle("rounded bg-yellow-100 text-yellow-700");
         }
-    },[subtitle])
+    },[selectedStatus])
 
-    const handleChangeStatus = () => {
-        console.log("Status: ", selectedValue);
-        saveStatus();
-        alert("You have successfully changed the status to: " + selectedValue);
-    }
+    const handleChangeStatus = (newStatus) => {
+        const userConfirmed = confirm("Are you sure to change the " + client.first_name + ' ' + client.last_name + " status to: " + newStatus + "?");
+        if (userConfirmed) {
+            setSelectedStatus(newStatus);
+            saveStatus(newStatus);
+        } else {
+            // Optional: Add logic for when the user cancels
+            console.log("User cancelled the status change.");
+        }
+    };
 
-    const saveStatus = async () => {
+
+    const saveStatus = async (newStatus) => {
 
         // sessionId and new Status
-        const newStatus = {
-            sessionId: sessionID,
-            status: selectedValue
+        const newStatusItem = {
+            sessionId: client.session_id,
+            status: newStatus
         };
 
         try {
             const res = await fetch(`/api/advisory_sessions`, {
                     method: 'PATCH',
                     headers: {  "Content-Type": "application/json" },
-                    body: JSON.stringify(newStatus)});
+                    body: JSON.stringify(newStatusItem)});
 
             if (!res.ok) {console.error("Failed to save change"); return;}
 
@@ -76,73 +79,52 @@ export default function ClientRow({
     // Display message
     const toggleExpanded = () => setExpanded((prev) => !prev);
 
-    const displayMessage = message
+    const displayMessage = client.message
         ? expanded
-        ? message
-        : message.slice(0, 50) + (message.length > 50 ? "..." : "")
+        ? client.message
+        : client.message.slice(0, 50) + (client.message.length > 50 ? "..." : "")
         : "Client has not sent a request yet";
 
     return(
-        <div className="mb-3 rounded-xl bg-[#F7EAE2] px-4 py-6">
-            <div className="flex flex-col gap-3 md:flex-row md:items-center md:justify-between">
-                {/* Left: name + subtitle */}
-                <div className="min-w-0">
-                    <div className="text-[20px] font-bold text-black text-left">{name}</div>
-                    <p className="text-gray-700 text-left">Session ID: {sessionID}</p>
-
-                    <div className="text-gray-700 text-left max-w-120">
-                        <p>Client Request: {displayMessage}</p>
-
-                        {/* Toggle button only if message exists and is long */}
-                        {message && message.length > 50 && (
-                            <button
-                            onClick={toggleExpanded}
-                            className="mt-1 text-sm text-grey-600 hover:underline"
-                            >
-                            {expanded ? "Show less" : "Show more"}
-                            </button>
-                        )}
-                    </div>
-                    
-                </div>
-
-                {/* Right: actions */}
-                <div className="flex flex-col shrink-0 items-end gap-3">
-                    {subtitle && (
-                        <select 
-                        className={`w-25 border rounded text-black ${subtitleStyle}`}
-                        id="dropdown" 
-                        value={selectedValue} 
-                        onChange={(e) => setSelectedValue(e.target.value)}>
-                            <option value="active" className="w-20 p-3 border rounded text-blue-500 bg-blue-100">Active</option>
-                            <option value="pending" className="w-20 p-3 border rounded text-yellow-500 bg-yellow-100">Pending</option>
-                            <option value="closed" className="w-20 p-3 border rounded text-red-500 bg-red-100">Closed</option>
-                        </select>
-                    )}
-                    <div className="flex gap-4">
-                        {/* Dark yellow message button with white text */}
-                        <button
-                            onClick={onMessage}
-                            className="rounded-md bg-yellow-600 px-4 py-2 text-sm font-semibold text-white
-                                    hover:bg-yellow-700 active:scale-[0.98] transition"
-                        >
-                            Message
-                        </button>
-
-                        {subtitle &&(
-                            <button 
-                                onClick={handleChangeStatus}
-                                className="w-25 rounded-md bg-[#44a237] px-4 py-2 text-sm font-semibold text-white
-                                hover:bg-green-700 active:scale-[0.98] transition">Save</button>
-                        )}
-                    
-                    </div>
-                    
-                    
-                    
-                    
-                </div>
+        <div className="flex flex-row justify-between text-black text-start border-b py-3">
+            <p className="w-1/12 px-2 py-1">{client.session_id}</p>
+            <p className="w-1/12 px-2 py-1">{client.first_name} {client.last_name}</p>
+            <p className="w-2/12 px-2 py-1">{client.email}</p>
+            <div className="w-4/12 px-2 py-1">
+                <p>{displayMessage}</p>
+                {client.message && client.message.length > 50 && (
+                    <button
+                    onClick={toggleExpanded}
+                    className="text-gray-500 hover:underline"
+                    >
+                    {expanded ? "Show less" : "Show more"}
+                    </button>
+                )}
             </div>
+            <div className="w-1/12">
+                {selectedStatus && (
+                    <select 
+                    className={`w-4/5 h-8 border rounded text-black ${statusStyle}`}
+                    id="dropdown" 
+                    value={selectedStatus} 
+                    onChange={(e) => handleChangeStatus(e.target.value)}>
+                        <option value="active" className="p-3 border rounded text-blue-700 bg-blue-100">Active</option>
+                        <option value="pending" className="p-3 border rounded text-yellow-700 bg-yellow-100">Pending</option>
+                        <option value="closed" className="p-3 border rounded text-red-700 bg-red-100">Closed</option>
+                    </select>
+                )}
+            </div>
+            <p className="w-2/12 px-2 py-1">10/5/2025 20:00 MTU</p>
+            <div className="w-1/12">
+                <button
+                    onClick={onMessage}
+                    className="rounded-md w-4/5 h-8 px-2 py-1 bg-yellow-600 text-sm font-semibold text-white
+                            hover:bg-yellow-700 active:scale-[0.98] transition"
+                >
+                    Message
+                </button>
+            </div>
+            
         </div>
     );
 }
