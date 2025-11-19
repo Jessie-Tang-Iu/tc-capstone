d1b9eb819de7f6ab047095cbce09dd2e36729ef7
Jessie Tang <Iu.Tang@edu.sait.ca>
2025-11-19 00:23:04 -0700

    fixing Spelling error in userRow component

--- app/components/adminDashboard/UserRow.js
+++ app/components/adminDashboard/UserRow.js
@@ -85,7 +85,7 @@ export default function UserRow({
               onClick={onInvestigate}
               className="rounded-md bg-green-600 px-4 py-2 text-sm font-semibold text-white hover:bg-green-700 active:scale-[0.98] transition"
             >
-              investigate
+              Investigate
             </button>
             )
           }
