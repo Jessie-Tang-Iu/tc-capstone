9daa8284d91c2fdc2c6aebc7ad80fe83adf55c70
DionEmary <emarydion15@gmail.com>
2025-12-01 21:16:57 -0700

    Fixing Layout Error

--- app/layout.js
+++ app/layout.js
@@ -19,15 +19,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {children}
+          <ClerkProvider>
+            {children}
+          </ClerkProvider>
         </body>
       </html>      
-    </ClerkProvider>
 
   );
 }
