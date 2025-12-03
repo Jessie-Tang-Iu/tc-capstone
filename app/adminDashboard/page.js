443c7722f22c0b8fa90caff34934fe097fd1f5d0
Lam Dao <lam.dao@edu.sait.ca>
2025-12-02 23:12:54 -0700

    fix deploy bug

--- app/adminDashboard/page.js
+++ app/adminDashboard/page.js
@@ -168,7 +168,7 @@ function AdminDashboardCore() {
             {tab === "users" && renderUsers()}
             {tab === "requests" && renderRequests()}
             {tab === "reports" && renderReports()}
-            {tab === "events" && <EventPanel />}
+            {tab === "events" && <EventsPanel />}
             {tab === "courses" && <CoursePage />}
           </div>
         </div>
